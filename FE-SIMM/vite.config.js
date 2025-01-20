d88c30f8fe9f7e46e1e963781eca2988e5eaf47b
ervinarosaa <ervinarosaaulia@gmail.com>
2025-01-20 10:01:53 +0700

    update

--- FE-SIMM/vite.config.js
+++ FE-SIMM/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://simm-kemenagsby.com/',
+        target: 'https://simm-kemenagsby.com/',
         changeOrigin: true,
       }
     }
